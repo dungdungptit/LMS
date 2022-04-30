b750e238f47fc4d31c7050871a97fd75299aff2a
dungdungptit <anhdungvk01@gmail.com>
2022-05-01 02:54:07 +0700

    home

---
+++ template/js/home.js
@@ -0,0 +1,16 @@
+var homeSlider = new Swiper('.home-slider', {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
